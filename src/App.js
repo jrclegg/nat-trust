07513abae22b72e292aa0b63a7aaf17606062ad0
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-04-10 20:58:25 +0100

    integrate national trust open data api and commit dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactMapboxGl from 'react-mapbox-gl';
+import axios from 'axios';
 
 const accessToken = "pk.eyJ1IjoiamNsZWdnIiwiYSI6ImNqdThyYmNjaDBjMTYzeW4wamZkNG8za28ifQ.P-f7Ail31NnEkbwxDdYGuA";
 const style = "mapbox://styles/mapbox/streets-v10";
@@ -14,6 +15,20 @@ const mapStyle = {
 };
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      openData: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://opendata.arcgis.com/datasets/202ec400dfe9471aaf257e4b6c956394_0.geojson`)
+      .then(res => {
+        const openData = res.data;
+        this.setState({ openData });
+      })
+  }
   render() {
     return (
       <Map
